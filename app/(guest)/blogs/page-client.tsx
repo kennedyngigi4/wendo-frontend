9a307f7878050707d1e807dbae4f36f1adf13a1d
kennedyngigi4 <kennedyngigi4@gmail.com>
2026-06-05 06:13:38 +0300

    Fix errors

--- app/(guest)/blogs/page-client.tsx
+++ app/(guest)/blogs/page-client.tsx
@@ -12,7 +12,7 @@ const BlogsClientPage = ({ blogs }: BlogsClientPageProps) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
 
-        {blogs.map((blog: BlogHomeModel) => (
+        {blogs?.map((blog: BlogHomeModel) => (
             <BlogCardComponent blog={blog} />
         ))}
 
